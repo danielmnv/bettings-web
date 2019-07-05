48dc359adf83a2f6ee0cb00a13d5c8f03909c961
Carlos Daniel Molina Vargas <danmnvx@gmail.com>
2019-07-04 21:37:43 -0500

    Configuración AngularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
+// Angular
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// AngularFire
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+// Sweet Alert 2
+
+// Ngx LoadingBar
+
+// Ng2 Charts
+
+// Animate
+
+// Components
 import { AppComponent } from './app.component';
 import { MaterialComponents } from './material.module';
 import { HousesComponent } from './components/houses/houses.component';
@@ -10,6 +25,12 @@ import { HomeComponent } from './components/home/home.component';
 import { HistoryComponent } from './components/history/history.component';
 import { CapitalComponent } from './components/capital/capital.component';
 import { BankComponent } from './components/bank/bank.component';
+
+// Services
+import { AuthService } from './services/auth.service';
+
+// Environments
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -24,9 +45,14 @@ import { BankComponent } from './components/bank/bank.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MaterialComponents
+    MaterialComponents,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDUUlIqjcxlSv4SfrlgSlfrrzOyRD-BCBM",
+    authDomain: "bettings-flow.firebaseapp.com",
+    databaseURL: "https://bettings-flow.firebaseio.com",
+    projectId: "bettings-flow",
+    storageBucket: "bettings-flow.appspot.com",
+    messagingSenderId: "417034595552",
+    appId: "1:417034595552:web:8ebc4ca0786400c8"
+  }
 };
 
 /*
