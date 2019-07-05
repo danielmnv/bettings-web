9e89194b67fd6f2a0ebe2ad121d27a2689af1fd1
Carlos Daniel Molina Vargas <danmnvx@gmail.com>
2019-07-04 21:11:57 -0500

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HistoryComponent } from './components/history/history.component';
+import { HousesComponent } from './components/houses/houses.component';
+import { CapitalComponent } from './components/capital/capital.component';
+import { BankComponent } from './components/bank/bank.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'houses', component: HousesComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'capital', component: CapitalComponent },
+  { path: 'bank', component: BankComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialComponents } from './material.module';
+import { HousesComponent } from './components/houses/houses.component';
+import { HomeComponent } from './components/home/home.component';
+import { HistoryComponent } from './components/history/history.component';
+import { CapitalComponent } from './components/capital/capital.component';
+import { BankComponent } from './components/bank/bank.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HousesComponent,
+    HomeComponent,
+    HistoryComponent,
+    CapitalComponent,
+    BankComponent
   ],
   imports: [
     BrowserModule,
