042d0da5ef4254fcc89c94cf62c2200eab2c1abb
Carlos Daniel Molina Vargas <danmnvx@gmail.com>
2019-07-08 00:16:03 -0500

    Prototipo de apuestas abiertas

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,8 +3,13 @@ import { MatButtonModule} from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatTabsModule} from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 @NgModule({
-  imports: [ MatButtonModule, MatToolbarModule, MatIconModule,MatTabsModule ],
-  exports: [ MatButtonModule, MatToolbarModule, MatIconModule,MatTabsModule ]
+  imports: [ MatButtonModule, MatToolbarModule, MatIconModule,MatTabsModule, MatCardModule, MatExpansionModule, MatDialogModule, MatTooltipModule ],
+  exports: [ MatButtonModule, MatToolbarModule, MatIconModule,MatTabsModule, MatCardModule, MatExpansionModule, MatDialogModule, MatTooltipModule ]
 })
 export class MaterialComponents { }
