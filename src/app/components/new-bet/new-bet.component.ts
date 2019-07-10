93d75cef73881ab30879b13189073687a7d30f5c
Carlos Daniel Molina Vargas <danmnvx@gmail.com>
2019-07-09 23:08:26 -0500

    Diseño y validación de formulario new bet

--- src/app/components/new-bet/new-bet.component.ts
+++ src/app/components/new-bet/new-bet.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { NgForm, FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-new-bet',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-bet.component.css']
 })
 export class NewBetComponent implements OnInit {
+  
+  private momio: number;
+  private amount: number;
+  private momioSign: boolean = true;
+  private newBetForm: FormGroup;
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<NewBetComponent>, private formBuilder: FormBuilder) {
+    this.newBetForm = this.formBuilder.group({
+      sportCtrl: ['', Validators.required],
+      houseCtrl: ['', Validators.required],
+      team1Ctrl: ['', Validators.required],
+      team2Ctrl: ['', Validators.required],
+      typeCtrl: ['', Validators.required],
+      eventDateCtrl: ['', Validators.required],
+      timeDateCtrl: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
+
+  }
+
+  addBetAndClose() {
+    
   }
 
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,9 +7,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  imports: [ MatButtonModule, MatToolbarModule, MatIconModule,MatTabsModule, MatCardModule, MatExpansionModule, MatDialogModule, MatTooltipModule ],
-  exports: [ MatButtonModule, MatToolbarModule, MatIconModule,MatTabsModule, MatCardModule, MatExpansionModule, MatDialogModule, MatTooltipModule ]
+  imports: [ MatButtonModule, MatToolbarModule, MatIconModule,MatTabsModule, MatCardModule, MatExpansionModule, MatDialogModule, MatTooltipModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule ],
+  exports: [ MatButtonModule, MatToolbarModule, MatIconModule,MatTabsModule, MatCardModule, MatExpansionModule, MatDialogModule, MatTooltipModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule ],
+  providers: [ MatDatepickerModule ]
 })
 export class MaterialComponents { }
