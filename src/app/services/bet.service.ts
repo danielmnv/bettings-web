9a310088a5af3785cbbaed2aef68dae8feb211c9
Carlos Daniel Molina Vargas <danmnvx@gmail.com>
2019-07-20 10:28:49 -0500

    Función para obtener todas las apuestas.

--- src/app/services/bet.service.ts
+++ src/app/services/bet.service.ts
@@ -3,19 +3,27 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/fire
 import { Bet } from '../interfaces/bet';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BetService {
 
+  public items: Observable<Bet []>;
   private betCollection: AngularFirestoreCollection<Bet>;
 
   constructor(private afs: AngularFirestore, private snack: MatSnackBar) {
+    // Get Bets Collection.
     this.betCollection = this.afs.collection<Bet>('bets');
   }
 
-  addBet(_bet: Bet) {
+  findBets(_uid: string): void {
+    // Store bets data on items.
+    this.items = this.betCollection.valueChanges();
+  }
+
+  addBet(_bet: Bet): Promise<any> {
     // Upload alert.
     Swal.fire({
       title: 'Guardando apuesta',
@@ -24,7 +32,7 @@ export class BetService {
     });
     Swal.showLoading();
 
-    // Push object into Firebase.
+    // Push object into Firebase. Return a DocumentReference if true.
     return this.betCollection.add(_bet)
       .then(res => {
         this.snack.open('Apuesta añadida', 'Cerrar', { duration: 8000 })
